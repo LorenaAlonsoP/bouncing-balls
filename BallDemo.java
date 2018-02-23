abea8dd6aaaab6da5cd5e33967198fc63c33239c
Lorena Alonso <lorenaalonsop26@gmail.com>
2018-02-23 08:56:13 +0100

    Modificado el metodo de mover y crear bolas

--- BallDemo.java
+++ BallDemo.java
@@ -11,43 +11,48 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private BouncingBall[] myBalls;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numberOfBalls)
     {
         int ground = 400;   // position of the ground line
+        
+        myBalls = new BouncingBall[numberOfBalls];
 
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
+        for(int i = 0; i < numberOfBalls; i++) {
+            BouncingBall ball = new BouncingBall(50 + (10 * i), 50, 10, Color.BLUE, ground, myCanvas);
+            ball.draw();
+            myBalls[i] = ball;
+        }
+        
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for(int i = 0; i<numberOfBalls; i++) {
+                myBalls[i].move();
+                if(myBalls[i].getXPosition() >= 550) {
+                    finished = true;
+                }
             }
+            
         }
+
     }
 }
